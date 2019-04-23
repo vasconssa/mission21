eabb07b0348e19074c06b6e284f3bf77d78018e2
Unknown <rapharvn@gmail.com>
2019-04-23 00:37:57 -0300

    Fix CxFreeze

--- source/setup.py
+++ source/setup.py
@@ -2,7 +2,8 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+# build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+build_exe_options = {"packages": ["numpy"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -12,6 +13,6 @@ if sys.platform == "win32":
 
 setup(  name = "Mission21",
         version = "1.0",
-        description = "Escape the gravity"
+        description = "Escape the gravity",
         options = {"build_exe": build_exe_options},
         executables = [Executable("Main.py", base=base)])
