49a82e432787e1c6a854e8acf445a23925fc7fef
Unknown <rapharvn@gmail.com>
2019-04-23 00:08:13 -0300

    Rename objects

--- source/SceneManager.py
+++ source/SceneManager.py
@@ -24,50 +24,50 @@ class SceneManager:
         planeta = prepare.GFX["assets"]['greenPlanet']
         rect = planeta.get_rect()
         w, h = rect.width, rect.height
-        planeta = pygame.transform.scale(planeta, (floor(0.3 * w), floor(0.3 * h)))
+        planetafase1 = pygame.transform.scale(planeta, (floor(0.3 * w), floor(0.3 * h)))
         wS, hS = self.screen.get_size()
         x = 0.5
         y = 0.5
         screenCoord = self.coord2Screen(x,y)
-        planet = Planet("teste2", planeta, 100001.0, screenCoord, 150.0)
-        scene1.addPlanet(planet)
+        planetfase1 = Planet("teste2", planetafase1, 100001.0, screenCoord, 150.0)
+        scene1.addPlanet(planetfase1)
 
         self.scenes.append(scene1)
 
         # Scene 2
         scene2 = Scene(self.screen)
-        planeta = prepare.GFX["assets"]['moonPlanet'].convert_alpha()
-        planeta2 = prepare.GFX["assets"]['rocketPlanet'].convert_alpha()
-        rect = planeta.get_rect()
+        planetafase2 = prepare.GFX["assets"]['moonPlanet'].convert_alpha()
+        planeta2fase2 = prepare.GFX["assets"]['rocketPlanet'].convert_alpha()
+        rect = planetafase2.get_rect()
         w, h = rect.width, rect.height
-        planeta = pygame.transform.scale(planeta, (floor(0.3 * w), floor(0.3 * h)))
-        planeta2 = pygame.transform.scale(planeta2, (floor(0.3 * w), floor(0.3 * h)))
+        planetafase2 = pygame.transform.scale(planetafase2, (floor(0.3 * w), floor(0.3 * h)))
+        planeta2fase2 = pygame.transform.scale(planeta2fase2, (floor(0.3 * w), floor(0.3 * h)))
         wS, hS = self.screen.get_size()
         x = 0.7
         y = 0.8
         screenCoord = self.coord2Screen(x,y)
-        planet = Planet("teste2", planeta, 100001.0, screenCoord, 250.0)
+        planetfase2 = Planet("teste2", planetafase2, 100001.0, screenCoord, 250.0)
         x = 0.4
         y = 0.3
         screenCoord = self.coord2Screen(x,y)
-        planet2 = Planet("teste2", planeta2, 100001.0, screenCoord, 200.0)
-        scene2.addPlanet(planet)
-        scene2.addPlanet(planet2)
+        planet2fase2 = Planet("teste2", planeta2fase2, 100001.0, screenCoord, 200.0)
+        scene2.addPlanet(planetfase2)
+        scene2.addPlanet(planet2fase2)
 
         self.scenes.append(scene2)
 
         # Scene 3
         scene3 = Scene(self.screen)
-        planeta = prepare.GFX["assets"]['sandPlanet'].convert_alpha()
-        rect = planeta.get_rect()
+        planetafase3 = prepare.GFX["assets"]['sandPlanet'].convert_alpha()
+        rect = planetafase3.get_rect()
         w, h = rect.width, rect.height
-        planeta = pygame.transform.scale(planeta, (floor(0.4 * w), floor(0.4 * h)))
+        planetafase3 = pygame.transform.scale(planetafase3, (floor(0.4 * w), floor(0.4 * h)))
         wS, hS = self.screen.get_size()
         x = 0.3
         y = 0.4
         screenCoord = self.coord2Screen(x,y)
-        planet = Planet("teste2", planeta, 100001.0, screenCoord, 150.0)
-        scene3.addPlanet(planet)
+        planetfase3 = Planet("teste2", planetafase3, 100001.0, screenCoord, 150.0)
+        scene3.addPlanet(planetfase3)
 
         self.scenes.append(scene3)
 
